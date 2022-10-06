166171fc03df2cac0356fafb5475bf1fc46b4f19
SwapnilDakhole <swapnildakhole242@gmail.com>
2022-10-06 14:49:07 +0530

    FIX : for Breaking change (tg's undefined)

--- pages/tg/index.tsx
+++ pages/tg/index.tsx
@@ -10,9 +10,20 @@ import Toast from "components/Toast";
 import attendanceColors from "components/AttendanceColor";
 import Table from "components/Tables/Table";
 import Link from "next/link";
+import { getSession } from "next-auth/react";
+import { prisma } from "lib/prisma";
 
 export const getServerSideProps = async (context: any) => {
-  return checkUserRoleAndRedirect(context, UserRole.TG, {});
+  const session = await getSession(context);
+  const tg = await prisma.tg.findUnique({
+    where: {
+      //@ts-ignore
+      id: session.id,
+    },
+  });
+  return checkUserRoleAndRedirect(context, UserRole.TG, {
+    extra: { tg: JSON.parse(JSON.stringify(tg)) },
+  });
 };
 
 type attendanceProps = {
@@ -22,7 +33,7 @@ type attendanceProps = {
   Attendances: Attendances;
 };
 
-const HomePage: NextPage = ({ user }: any) => {
+const HomePage: NextPage = ({ tg }: any) => {
   const [attendances, setAttendances] = useState<attendanceProps[]>([]);
   const [toast, setToast] = useState<ToastParams>();
 
@@ -49,33 +60,34 @@ const HomePage: NextPage = ({ user }: any) => {
         {toast && (
           <Toast
             type={toast.type}
-            className='mb-5'
+            className="mb-5"
             open={true}
-            setOpen={() => setToast(undefined)}>
+            setOpen={() => setToast(undefined)}
+          >
             {toast.message}
           </Toast>
         )}
       </div>
-      <div className='h-screen'>
-        <h1 className='font-semibold text-lg mb-4'>
+      <div className="h-screen">
+        <h1 className="font-semibold text-lg mb-4">
           Welcome, {tg.gender == "Male" ? `Mr. ${tg.name}` : `Ms. ${tg.name}`}!
         </h1>
-        <div className='flex overflow-x-scroll '>
-          <div className='shrink-0'>
-            <Card title='All Students' value={attendances.length} />
+        <div className="flex overflow-x-scroll ">
+          <div className="shrink-0">
+            <Card title="All Students" value={attendances.length} />
           </div>
-          <div className='shrink-0'>
+          <div className="shrink-0">
             <Card
-              title='Weak Students'
+              title="Weak Students"
               value={
                 //@ts-ignore
                 parseInt(attendances.length / 3)
               }
             />
           </div>
-          <div className='shrink-0'>
+          <div className="shrink-0">
             <Card
-              title='Avg. Students'
+              title="Avg. Students"
               value={
                 //@ts-ignore
                 parseInt(attendances.length / 3)
@@ -84,10 +96,10 @@ const HomePage: NextPage = ({ user }: any) => {
           </div>
         </div>
 
-        <div className='pt-11'>
+        <div className="pt-11">
           <Table
             refresh={getAttendance}
-            title='Attendance per Lecture'
+            title="Attendance per Lecture"
             headings={[
               "name",
               "rollNo",
@@ -97,73 +109,80 @@ const HomePage: NextPage = ({ user }: any) => {
               "lecture4",
               "lecture5",
               "lecture6",
-            ]}>
+            ]}
+          >
             {attendances.map((attendance) =>
               //@ts-ignore
               attendances.Attendances ? (
                 <tr key={attendance.Attendances.id}>
-                  <td className='pl-5 p-2 whitespace-nowrap text-violet-500'>
+                  <td className="pl-5 p-2 whitespace-nowrap text-violet-500">
                     <Link href={`tg/students/${attendance.id}`}>
                       <a>{attendance.name}</a>
                     </Link>
                   </td>
-                  <td className='p-2 whitespace-nowrap'>{attendance.rollNo}</td>
-                  <td className='p-2 whitespace-nowrap'>
-                    <div className='flex items-center'>
+                  <td className="p-2 whitespace-nowrap">{attendance.rollNo}</td>
+                  <td className="p-2 whitespace-nowrap">
+                    <div className="flex items-center">
                       <div
                         className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
                           attendance.Attendances.lecture1
-                        )}`}>
+                        )}`}
+                      >
                         {attendance.Attendances.lecture1}
                       </div>
                     </div>
                   </td>
-                  <td className='p-2 whitespace-nowrap'>
-                    <div className='flex items-center'>
+                  <td className="p-2 whitespace-nowrap">
+                    <div className="flex items-center">
                       <div
                         className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
                           attendance.Attendances.lecture2
-                        )}`}>
+                        )}`}
+                      >
                         {attendance.Attendances.lecture2}
                       </div>
                     </div>
                   </td>
-                  <td className='p-2 whitespace-nowrap'>
-                    <div className='flex items-center'>
+                  <td className="p-2 whitespace-nowrap">
+                    <div className="flex items-center">
                       <div
                         className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
                           attendance.Attendances.lecture3
-                        )}`}>
+                        )}`}
+                      >
                         {attendance.Attendances.lecture3}
                       </div>
                     </div>
                   </td>
-                  <td className='p-2 whitespace-nowrap'>
-                    <div className='flex items-center'>
+                  <td className="p-2 whitespace-nowrap">
+                    <div className="flex items-center">
                       <div
                         className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
                           attendance.Attendances.lecture4
-                        )}`}>
+                        )}`}
+                      >
                         {attendance.Attendances.lecture4}
                       </div>
                     </div>
                   </td>
-                  <td className='p-2 whitespace-nowrap'>
-                    <div className='flex items-center'>
+                  <td className="p-2 whitespace-nowrap">
+                    <div className="flex items-center">
                       <div
                         className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
                           attendance.Attendances.lecture5
-                        )}`}>
+                        )}`}
+                      >
                         {attendance.Attendances.lecture5}
                       </div>
                     </div>
                   </td>
-                  <td className='p-2 whitespace-nowrap'>
-                    <div className='flex items-center'>
+                  <td className="p-2 whitespace-nowrap">
+                    <div className="flex items-center">
                       <div
                         className={`mt-1.5 inline-flex font-medium rounded-full text-center px-2 py-0.3 ${attendanceColors(
                           attendance.Attendances.lecture6
-                        )}`}>
+                        )}`}
+                      >
                         {attendance.Attendances.lecture6}
                       </div>
                     </div>
@@ -172,12 +191,12 @@ const HomePage: NextPage = ({ user }: any) => {
               ) : (
                 //@ts-ignore
                 <tr key={attendance.name}>
-                  <td className='pl-5 p-2 whitespace-nowrap text-violet-500'>
+                  <td className="pl-5 p-2 whitespace-nowrap text-violet-500">
                     <Link href={`/TG/students/${attendance.id}`}>
                       <a>{attendance.name}</a>
                     </Link>
                   </td>
-                  <td className='p-2 whitespace-nowrap'>{attendance.rollNo}</td>
+                  <td className="p-2 whitespace-nowrap">{attendance.rollNo}</td>
                 </tr>
               )
             )}
